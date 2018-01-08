09535ccde2d3b64ab9a269e2f6fff1ac48f2d0f7
joenathan <cyborg.sasuke92@gmail.com>
2018-01-08 14:21:28 +0700

    working on welcome activity

--- app/src/main/java/app/ubk/mychat/WelcomeActivity.java
+++ app/src/main/java/app/ubk/mychat/WelcomeActivity.java
@@ -1,5 +1,6 @@
 package app.ubk.mychat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,7 +9,41 @@ public class WelcomeActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_welcome );
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        Thread thread = new Thread(  )
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    sleep( 3000 );
+                }
+
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+
+                finally
+                {
+                    Intent mainIntent = new Intent( WelcomeActivity.this, MainActivity.class );
+                    startActivity( mainIntent );
+                }
+            }
+        };
+        thread.start();
+
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+
+        finish();
     }
 }
+
